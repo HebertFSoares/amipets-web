6b0928c08b28d58af38ddb017bcc5e58dca7bc5d
bapGabriel <bapgabriel@outlook.com>
2024-11-28 13:02:55 -0300

    feat: adiciona burger menu

---
+++ src/components/BurgerMenu.jsx
@@ -0,0 +1,68 @@
+import { useAuth } from "@/hooks/auth/AuthProvider";
+import { Popover, PopoverBackdrop, PopoverButton, PopoverPanel } from "@headlessui/react";
+import { CiMenuBurger } from "react-icons/ci";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+
+export default function BurgerMenu() {
+  const categories = [
+    {
+      label: "Eu",
+      links: [
+        {linkTo: "/user/", label: "Minhas adoções"},
+        {linkTo: "/user/", label: "Meus pets favoritos"},
+        {linkTo: "/user/", label: "Buscar pets"},
+        {linkTo: "/user/", label: "Editar perfil"}
+      ]
+    },
+    {
+      label: "Administração",
+      links: [
+        {linkTo: "/admin/", label: "Painel de administração"},
+        {linkTo: "/admin/", label: "Painel de pets"},
+        {linkTo: "/admin/", label: "Painel de adoções"},
+        {linkTo: "/admin/", label: "Painel de adotantes"}
+      ]
+    },
+    {
+      links: [
+        {linkTo: "#", label: "Logout"}
+      ]
+    }
+  ]
+  const user = useAuth();
+
+  return (
+    <Popover className="relative group">
+      <PopoverButton>
+        <Button className="bg-primary-400">
+          <CiMenuBurger className="transition duration-200 ease-in group-data-[open]:rotate-90" transition/>
+        </Button>
+      </PopoverButton>
+      <PopoverBackdrop className="fixed inset-0 bg-black/25 transition duration-100 data-[closed]:opacity-0" transition/>
+      <PopoverPanel anchor="bottom" className="flex flex-col space-y-2 bg-white p-4 rounded-lg shadow-lg transition duration-200 ease-out data-[closed]:-translate-y-4 data-[closed]:opacity-0" transition>
+        {categories.map((category, index) => (
+          <div key={index} className="bg-primary-400 rounded-md p-4 flex flex-col text-right">
+            <div className="flex flex-row align-middle space-x-2">
+              <div className="flex-auto bg-black h-0.5 self-center"></div>
+              {category.label && 
+                <p className="font-bold">{category.label}</p>
+              }
+            </div>
+              {category.links.map((link, index) => (
+                <Link to={link.linkTo} className="text-text-900 underline hover:text-text-500" onClick={link.label === "Logout" ? user.logout : null }>{link.label}</Link>
+              ))}
+          </div>
+        ))}
+      </PopoverPanel>
+    </Popover>
+  )
+}
+
+function Category({ children }) {
+  return (
+    <div className="bg-primary-400 rounded-md p-4 flex flex-col text-right text-text-900 underline">
+      {children}
+    </div>
+  )
+}
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,25 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { useAuth } from "@/hooks/auth/AuthProvider";
+import { useState } from "react";
+import BurgerMenu from "./BurgerMenu";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const user = useAuth();
 
   return (
     <div className="bg-primary-600 border-b-2 border-primary-900 flex flex-row justify-between items-center py-4 px-16">
       <div>
-        <img src="src/assets/logo.svg" alt="" className="w-36" />
+        <Link to="/">
+          <img src="src/assets/logo.svg" alt="" className="w-36" />
+        </Link>
       </div>
       <div className="space-x-2">
-        <Button>
+        <Button className="bg-primary-400">
           <Link to="/">Início</Link>
         </Button>
-        <Button>
+        <Button className="bg-primary-400">
           <Link to="/pets">Pets</Link>
         </Button>
       </div>
@@ -30,6 +35,7 @@ export default function Header() {
       }
       {user.token &&
         <div className="flex space-x-2">
+          <BurgerMenu />
           <Button className="bg-primary-400">
             {/* TODO: Trocar pelo hamburguer e fazer o modal com todas as opções e afins */}
             <Link to="#" onClick={user.logout}>Logout</Link>
