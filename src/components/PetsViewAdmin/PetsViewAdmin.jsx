ec641394f03938b42f4641faa7d1fcee2157dcbb
roddorneles <rodrigodornelesferreira@usp.br>
2024-11-29 12:15:12 -0300

    feat: Cria componente de resumo de pets

--- src/components/PetsViewAdmin/PetsViewAdmin.jsx
+++ src/components/PetsViewAdmin/PetsViewAdmin.jsx
@@ -22,6 +22,7 @@ import { useQuery } from "@tanstack/react-query";
 import { api } from "@/lib/apiWrapper";
 import { sendPetToAPI } from "@/services/sendPetToAPI";
 import { updatePet } from "@/services/updatePet";
+import { PetsResume } from "../PetsResume/PetsResume";
 
 export function PetsViewAdmin() {
 
@@ -70,97 +71,103 @@ export function PetsViewAdmin() {
 
     return (
 
-        <div className="w-full p-8">
-            <div className="max-w-5xl space-y-4">
-                <h1 className="text-3xl font-bold ">Pets</h1>
+        <div className="w-full p-10 flex flex-col">
 
-                <div className="">
-                    <PetsFilters></PetsFilters>
-                </div >
+            <h1 className="text-3xl font-bold mb-4">Pets</h1>
 
-                <div className="border rounded-lg p-2">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead>ID</TableHead>
-                                <TableHead>Foto</TableHead>
-                                <TableHead>Nome</TableHead>
-                                <TableHead>Espécie</TableHead>
-                                <TableHead >Data de Nascimento</TableHead>
-                                <TableHead>Status</TableHead>
-                                <TableHead>Tamanho</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
+            <div className="flex justify-between space-x-10">
+                <div className="flex flex-col flex-grow space-y-4">
+                    <PetsFilters />
+                    <div className="border rounded-lg p-2">
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>ID</TableHead>
+                                    <TableHead>Foto</TableHead>
+                                    <TableHead>Nome</TableHead>
+                                    <TableHead>Espécie</TableHead>
+                                    <TableHead >Data de Nascimento</TableHead>
+                                    <TableHead>Status</TableHead>
+                                    <TableHead>Tamanho</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
 
-                            {
-                                pets && pets.map((pet) => {
-                                    return <TableRow key={pet.id}>
-                                        <TableCell>{pet.id}</TableCell>
-                                        <TableCell>
-                                            <div className="w-20 h-20 rounded-3xl overflow-hidden flex justify-center items-center">
-                                                <img src={pet.foto} alt={`foto de ${pet.nome}`} className="w-full h-full object-cover" />
-                                            </div>
-                                        </TableCell>
-                                        <TableCell>{pet.nome}</TableCell>
-                                        <TableCell>{capitalize(pet.especie)}</TableCell>
-                                        <TableCell>{getFormattedDate(pet.dataNascimento)}</TableCell>
-                                        <TableCell>{getFormattedPetStatus(pet.status)}</TableCell>
-                                        <TableCell>{capitalize(pet.tamanho)}</TableCell>
-                                        <TableCell>
-                                            <Button variant="ghost">
-                                                <Ellipsis className="w-4" />
-                                            </Button>
-                                        </TableCell>
-                                        <TableCell>
-                                            <PetFormDialog
-                                                key={pet.id}
-                                                description={"Edite as informações do pet no sistema. Clique em salvar quando tiver finalizado."}
-                                                title={"Editar pet"}
-                                                onSubmit={editPet}
-                                                initialValues={{
-                                                    id: pet.id,
-                                                    name: pet.nome,
-                                                    specie: pet.especie,
-                                                    birthDate: getFormattedDate(pet.dataNascimento),
-                                                    size: capitalize(pet.tamanho),
-                                                    description: pet.descricao,
-                                                    status: getFormattedPetStatus(pet.status),
-                                                    personalities: pet.personalidade && pet.personalidade.map((x) => { return { value: x } })
-                                                }}
-                                            >
-                                                <Button type="button" variant="ghost"><PencilIcon className="w-4" /></Button>
-                                            </PetFormDialog>
+                                {
+                                    pets && pets.map((pet) => {
+                                        return <TableRow key={pet.id}>
+                                            <TableCell>{pet.id}</TableCell>
+                                            <TableCell>
+                                                <div className="w-20 h-20 rounded-3xl overflow-hidden flex justify-center items-center">
+                                                    <img src={pet.foto} alt={`foto de ${pet.nome}`} className="w-full h-full object-cover" />
+                                                </div>
+                                            </TableCell>
+                                            <TableCell>{pet.nome}</TableCell>
+                                            <TableCell>{capitalize(pet.especie)}</TableCell>
+                                            <TableCell>{getFormattedDate(pet.dataNascimento)}</TableCell>
+                                            <TableCell>{getFormattedPetStatus(pet.status)}</TableCell>
+                                            <TableCell>{capitalize(pet.tamanho)}</TableCell>
+                                            <TableCell>
+                                                <Button variant="ghost">
+                                                    <Ellipsis className="w-4" />
+                                                </Button>
+                                            </TableCell>
+                                            <TableCell>
+                                                <PetFormDialog
+                                                    key={pet.id}
+                                                    description={"Edite as informações do pet no sistema. Clique em salvar quando tiver finalizado."}
+                                                    title={"Editar pet"}
+                                                    onSubmit={editPet}
+                                                    initialValues={{
+                                                        id: pet.id,
+                                                        name: pet.nome,
+                                                        specie: pet.especie,
+                                                        birthDate: getFormattedDate(pet.dataNascimento),
+                                                        size: capitalize(pet.tamanho),
+                                                        description: pet.descricao,
+                                                        status: getFormattedPetStatus(pet.status),
+                                                        personalities: pet.personalidade && pet.personalidade.map((x) => { return { value: x } })
+                                                    }}
+                                                >
+                                                    <Button type="button" variant="ghost"><PencilIcon className="w-4" /></Button>
+                                                </PetFormDialog>
 
-                                        </TableCell>
-                                        <TableCell>
-                                            <Trash2 className="w-4" />
-                                        </TableCell>
-                                    </TableRow>
-                                })
-                            }
-                        </TableBody>
-                    </Table>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Trash2 className="w-4" />
+                                            </TableCell>
+                                        </TableRow>
+                                    })
+                                }
+                            </TableBody>
+                        </Table>
+                    </div>
                 </div>
 
-                <PetFormDialog
-                    description={"Crie um novo pet no sistema. Clique em salvar quando tiver finalizado."}
-                    title={"Cadastrar pet"}
-                    onSubmit={createPet}
-                    initialValues={{
-                        id: "",
-                        name: "",
-                        specie: "",
-                        birthDate: "",
-                        description: "",
-                        personalities: [],
-                        status: "Livre"
-                    }}
-                >
-                    <Button type="button"> <PlusCircle />Cadastrar Pet</Button>
-                </PetFormDialog>
+                <div className="flex flex-col flex-grow space-y-4">
+                    <PetsResume />
+                    <PetFormDialog
+                        description={"Crie um novo pet no sistema. Clique em salvar quando tiver finalizado."}
+                        title={"Cadastrar pet"}
+                        onSubmit={createPet}
+                        initialValues={{
+                            id: "",
+                            name: "",
+                            specie: "",
+                            birthDate: "",
+                            description: "",
+                            personalities: [],
+                            status: "Livre"
+                        }}
+                    >
+                        <Button type="button" className="bg-primary-500 hover:bg-primary-400 ">
+                            <PlusCircle />Cadastrar Pet
+                        </Button>
+                    </PetFormDialog>
+                </div>
 
-            </div >
+            </div>
+
         </div>
 
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,73 +1,76 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-  	extend: {
-  		colors: {
-  			text: {
-				'900': '#FEFEFE',
-  				'500': '#CCCCCC',
-  				DEFAULT: '#000000'
-  			},
-  			background: 'hsl(var(--background))',
-  			primary: {
-  				'50': '#0f1406',
-  				'100': '#1e270c',
-  				'200': '#3b4f17',
-  				'300': '#597623',
-  				'400': '#779d2f',
-  				'500': '#94c43b',
-  				'600': '#aad062',
-  				'700': '#bfdc89',
-  				'800': '#d4e8b0',
-  				'900': '#eaf3d8',
-  				'950': '#f4f9eb',
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
+	darkMode: ["class"],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			boxShadow: {
+				'center-sm': '0px 2px 11px 0px rgba(0, 0, 0, 0.1)'
+			},
+			colors: {
+				text: {
+					'900': '#FEFEFE',
+					'500': '#CCCCCC',
+					DEFAULT: '#000000'
+				},
+				background: 'hsl(var(--background))',
+				primary: {
+					'50': '#0f1406',
+					'100': '#1e270c',
+					'200': '#3b4f17',
+					'300': '#597623',
+					'400': '#779d2f',
+					'500': '#94c43b',
+					'600': '#aad062',
+					'700': '#bfdc89',
+					'800': '#d4e8b0',
+					'900': '#eaf3d8',
+					'950': '#f4f9eb',
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				foreground: 'hsl(var(--foreground))',
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				chart: {
+					'1': 'hsl(var(--chart-1))',
+					'2': 'hsl(var(--chart-2))',
+					'3': 'hsl(var(--chart-3))',
+					'4': 'hsl(var(--chart-4))',
+					'5': 'hsl(var(--chart-5))'
+				}
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			}
+		}
+	},
 };
